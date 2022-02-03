2a3edfe1dcf81847bb339213a7659fdac9a36f2d
Smitha Perumalil <smitha_perumalil@aisreview.com>
2022-02-03 20:06:06 +0000

    Initial commit

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS TBL_COUNTRIES;
+DROP TABLE IF EXISTS TBL_STUDENTS;
+
+
+CREATE TABLE TBL_COUNTRIES (
+                               id  int  PRIMARY KEY,
+                               country_name  VARCHAR(250) not null
+);
+
+CREATE TABLE TBL_STUDENTS (
+                               id INT   PRIMARY KEY,
+                               first_name VARCHAR(250) NOT NULL,
+                               last_name VARCHAR(250) NOT NULL,
+                               country_id  int null
+);
+
+
+ALTER TABLE TBL_STUDENTS
+    ADD FOREIGN KEY (country_id)
+        REFERENCES TBL_COUNTRIES(id);
+
